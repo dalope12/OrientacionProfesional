a094960957d7cf231a93512f4ae3fd4fea5e74ae
dalope12 <da.lope12@gmail.com>
2017-12-01 19:03:55 -0500

    Cálculo de la prueba

--- CONTROLADOR/aspirante.php
+++ CONTROLADOR/aspirante.php
@@ -1,9 +1,25 @@
 <?php
-    $Nombre = $_POST['Nombre'];
+    /*$Nombre = $_POST['Nombre'];
     $Apellido = $_POST['Apellido'];
-    $Correo = $_POST['Correo'];
+    $Correo = $_POST['Correo'];*/
+    
+    $data = json_decode(stripslashes($_POST['data']));
+    $data2 = json_decode(stripslashes($_POST['parametros']));
+    $nombre = $data2->Nombre;
+    /*echo $Nombre."<br>";
+    echo $Apellido."<br>";
+    echo $Correo."<br>";*/
+    /*foreach($data2 as $dd){
+        echo $dd."<br>";
+    }/*
+    
+    foreach($data as $d){
+        echo $d."<br>";
+    }*/
+    echo $data[8]."<br>";
+    echo $nombre;
 
-    echo $Nombre;
-    echo $Apellido;
-    echo $Correo;
+
+
+
 ?>
